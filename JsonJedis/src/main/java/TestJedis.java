718cd42511b1b3b8cbb61586854624b58c555d97
hcd <hcd@xps4.hcdata.local>
2016-05-12 15:55:31 -0700

    Parse JSON obj file to string

--- JsonJedis/src/main/java/TestJedis.java
+++ JsonJedis/src/main/java/TestJedis.java
@@ -8,16 +8,34 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
 import java.io.FileWriter;
+import java.io.BufferedReader;
 
 public class TestJedis {
-  public static void main(String[] args) {
+    public static String readFile(String fileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append("\n");
+                line = br.readLine();
+            }
+            return sb.toString();
+        } finally {
+            br.close();
+        }
+    }
+
+  public static void main(String[] args) throws IOException {
     //Connecting to Redis on localhost
-    Jedis jedis = new Jedis("localhost", 22121);
+    Jedis jedis = new Jedis("xps4", 22121);
   //  Jedis jedis = new Jedis("localhost");
     //adding a new key
-      System.out.println(jedis.hcdsetproxy("key777", "myvalue55555"));
+      //System.out.println(jedis.hcdsetproxy("key777", "myvalue55555"));
     //getting the key value
-    System.out.println(jedis.hcdgetproxy("key456"));
+    //System.out.println(jedis.hcdgetproxy("key456"));
 
     //generating a json object
     JSONObject obj = new JSONObject();
@@ -41,13 +59,35 @@ public class TestJedis {
     } catch (IOException e) {
       e.printStackTrace();
     }
+      //reading json object from a file
+     // String buf = readFile("confJSON");
+      String buf = readFile("confHE");
+      System.out.println(buf);
 
+      JSONParser parser = new JSONParser();
+      try {
+          System.out.println(jedis.hcdsetproxy("test", parser.parse(buf).toString()));
+      }
+      catch (  Exception e) {
+          throw new AssertionError("not a valid JSON object");
+      }
+
+
+
+ //   jedis.hcdsetproxy("test", buf);
     //testing redis cmd: ping, info, set, get,
     //                hcdsetproxy, hcdgetproxy
-    System.out.println(jedis.ping());
+//    System.out.println(jedis.ping());
 //    System.out.println(jedis.info());
-    jedis.set("keys1", obj.toString());
-    System.out.println(jedis.hcdsetproxy("keyhcd", obj.toString()));
-    System.out.println("from redis server: "+jedis.get("keys1"));
+//    jedis.set("keys1", obj.toString());
+//    System.out.println(jedis.hcdsetproxy("keyhcd", obj.toString()));
+//    System.out.println("from redis server: "+jedis.get("keys1"));
+
+      try {
+          jedis.quit();
+      } catch (Exception e) {
+          // ignore the exception node, so that all other normal nodes can release all connections.
+      }
+
   }
 }
